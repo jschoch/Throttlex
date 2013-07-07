5ee2e29294536f38afda070ec0f3a39eb99db135
EC2 Default User <ec2-user@ip-10-40-65-192.ec2.internal>
2013-07-07 16:18:18 +0000

    tests passing, still need supervisor to work

--- lib/throttlex.ex
+++ lib/throttlex.ex
@@ -14,15 +14,15 @@ defmodule Throttlex.Server do
 		throttle(:w1s)
 	end
 	def throttle(name) do
-          Lager.info "Throttle: Starting at #{inspect :erlang.time}"
-          result = :gen_server.call :throttlex, {:throttle,name}
-          Lager.info "sleeping for: #{result}"
-          timer_result = :timer.sleep(result)
-	  Lager.info "Timer Result was: #{timer_result}"
-          :gen_server.cast :throttlex, {:done,name}
-          Lager.info "Throttle: ending at #{inspect :erlang.time}"
-	  result
-        end
+      Lager.info "Throttle: Starting at #{inspect :erlang.time}"
+      result = :gen_server.call :throttlex, {:throttle,name}
+      Lager.info "sleeping for: #{result}"
+      timer_result = :timer.sleep(result)
+	  	Lager.info "Timer Result was: #{timer_result}"
+      :gen_server.cast :throttlex, {:done,name}
+      Lager.info "Throttle: ending at #{inspect :erlang.time}"
+	  	result
+	end
 	
 	def up_tex(tex) do
 		tex.update wait: tex.wait + tex.timeout, queue: tex.queue + 1
@@ -51,19 +51,19 @@ defmodule Throttlex.Server do
 		{:noreply,new_state}
 	end
 	
-        def handle_call({:throttle,name},_from,state) do
+	def handle_call({:throttle,name},_from,state) do
 		case state.by_name[name] do
 		  nil -> 
-			Lager.error "can't find throttle named: #{inspect name}"	
-			Lager.error "available throttles: #{inspect ListDict.keys(state.by_name)}"
-			{:reply,0,state}
+				Lager.error "can't find throttle named: #{inspect name}"	
+				Lager.error "available throttles: #{inspect ListDict.keys(state.by_name)}"
+				{:reply,0,state}
 		  tex -> 
-                	tex = up_tex(tex)
-			state = update_state(state,tex)
-			Lager.info "wait? #{tex.wait}"
-                	{:reply,tex.wait,state}
+      	tex = up_tex(tex)
+				state = update_state(state,tex)
+				Lager.info "wait? #{tex.wait}"
+        {:reply,tex.wait,state}
 		end
-        end
+	end
         def handle_cast({:done,name},state) do
 		case state.by_name[name] do
 			nil -> Lager.error "cast :done no name #{name}"
@@ -75,14 +75,7 @@ defmodule Throttlex.Server do
                 		{:noreply,state}
 			end
         end
-        def work_it do
-          lc x inlist [1,2,3,4,5,6] do
-                spawn(Throttle,:throttlex,[])
-          end
-          lc x inlist [1,2,3,4,5,6] do
-                spawn(Throttle,:throttlex,[])
-          end
-        end
+        
         def init(state) do
 		Lager.info "Starting Throttle Server"
 		Lager.info inspect state
@@ -92,7 +85,7 @@ defmodule Throttlex.Server do
                 :gen_server.call :throttlex, :stop
         end
 	def handle_call(:stop,_from,state) do
-                IO.puts "shutting down"
+                IO.puts "Throttlex.Server.stop: shutting down"
                 {:stop,:normal,:shutdown_ok,state}
         end
 end
--- test/throttlex_test.exs
+++ test/throttlex_test.exs
@@ -1,68 +1,87 @@
 Code.require_file "test_helper.exs", __DIR__
-
+defmodule TWork do
+	require Lager
+	def go(name) do
+		result = Throttlex.Server.throttle(name)	
+		Lager.info "TWork.go: #{inspect self} result: #{result}"
+	end
+  def mon([]) do
+ 		Lager.info "Processes all done" 
+  end	
+  def mon([h|t]) do
+  	thing = :erlang.monitor(:process, h)	
+  	Lager.info inspect thing
+  	mon t
+  end
+	def start(name) do
+		pids = lc x inlist Enum.to_list 1..10 do
+			Lager.info "TWork.start: Starting spawn #{x}"
+			pid = spawn(TWork,:go,[name])	
+			Lager.info "TWork.start Ending spawn #{x}"
+			pid
+		end	
+		IO.puts inspect pids
+		mon(pids)	
+	end
+end
 defmodule ThrottlexTest do
   use ExUnit.Case
   alias Throttlex.Server, as: T
   alias Throttlex.Super, as: S
   def startup do
-
+		IO.puts "starting"
   end
   def teardown do
+  	IO.puts "shutting down"
     T.stop
-  end
-  def work_it do
-    lc x inlist [1,2,3,4,5,6] do
-      spawn(Throttlex.Server,:throttle,[:fast])
-    end
-    lc x inlist [1,2,3,4,5,6] do
-      spawn(Throttlex.Server,:throttle,[:fast])
-    end
   end
   test "super works on crash" do
      #S.start_link(1)
      #T.throttle(:fail)
-    assert(false)
+    #assert(false)
   end
   test "goes fast" do
-	T.stop
-	tex = S.Tex.new
-	fast = S.Tex.new(name: :fast,timeout: 1000)
-	crap = [{tex.name,tex}]
-	#crap = ListDict.put(crap,fast.name,fast)
-        state = S.Throttles.new(by_name: crap, count: 1)
-	state = T.add_tex(state,fast)
-        T.start_link(state)
-	work_it
+		T.stop
+		tex = S.Tex.new
+		fast = S.Tex.new(name: :fast,timeout: 100)
+		crap = [{tex.name,tex}]
+		#crap = ListDict.put(crap,fast.name,fast)
+    state = S.Throttles.new(by_name: crap, count: 1)
+		state = T.add_tex(state,fast)
+    T.start_link(state)
+		res = TWork.start(:fast)
+		#res2 = TWwork.start(:fast)
+		:timer.sleep(1100)
   end
   test "record tests" do
-	tex = S.Tex.new
-	state = S.Throttles.new(by_name: [{tex.name, tex}])
-	assert(tex.name == :w1s)
-	tex = T.up_tex(tex)
-	assert(tex.wait == 1000)
-	tex = T.down_tex(tex)
-	assert(tex.wait == 0)
-	tst = state.by_name[:w1s]	
-	assert(tex == tst)
-	tex2 = S.Tex.new(name: :foo, timeout: 1500)
-	state = T.add_tex(state,tex2)
-	assert(:foo == state.by_name[:foo].name)
-	state = T.update_state(state,tex)
-	keys = ListDict.keys(state.by_name)
-	assert([:w1s,:foo] == keys)
+		tex = S.Tex.new
+		state = S.Throttles.new(by_name: [{tex.name, tex}])
+		assert(tex.name == :w1s)
+		tex = T.up_tex(tex)
+		assert(tex.wait == 1000)
+		tex = T.down_tex(tex)
+		assert(tex.wait == 0)
+		tst = state.by_name[:w1s]	
+		assert(tex == tst)
+		tex2 = S.Tex.new(name: :foo, timeout: 1500)
+		state = T.add_tex(state,tex2)
+		assert(:foo == state.by_name[:foo].name)
+		state = T.update_state(state,tex)
+		keys = ListDict.keys(state.by_name)
+		assert([:w1s,:foo] == keys)
 	
 
   end
   test "register new throttle" do
-	foo = S.Tex.new(name: :foo,timeout: 15)
-	tex = S.Tex.new
-    	state = S.Throttles.new(by_name: [{tex.name, tex}], count: 1)
-	T.start_link(state)
-	state = T.add_tex(state,foo)
-	T.update(state)
-	result = T.throttle(:foo)
-	IO.puts("result: #{inspect result}")
-	assert(15 == result)
+		foo = S.Tex.new(name: :register_new_throttle,timeout: 15)
+		tex = S.Tex.new
+		state = S.Throttles.new(by_name: [{tex.name, tex}], count: 1)
+		T.start_link(state)
+		state = T.add_tex(state,foo)
+		T.update(state)
+		result = T.throttle(:register_new_throttle)
+		IO.puts("result: #{inspect result}")
+		assert(15 == result)
 	
   end
   test "default throttle works" do
@@ -71,7 +90,6 @@ defmodule ThrottlexTest do
     state = S.Throttles.new(by_name: [{tex.name, tex}], count: 1)
     T.start_link(state)
     wait = T.throttle
-    IO.puts "FUCKYOU: #{wait}"
     assert(1000 == wait)
   end
 end
